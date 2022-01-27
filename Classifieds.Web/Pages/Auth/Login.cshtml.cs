7f9d1f777a6188798b2c7e4c6fd92cec51efe909
Maro de Melo <maromelo@tempus.digital>
2022-01-27 16:33:06 -0300

    Add login and logout functionality

--- Classifieds.Web/Pages/Auth/Login.cshtml.cs
+++ Classifieds.Web/Pages/Auth/Login.cshtml.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -41,6 +44,31 @@ namespace Classifieds.Web.Pages.Auth
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
+
+            //real database call to check if username and password are correct goes here!
+
+            if(Input.Email == "admin@test.com" && Input.Password == "maro")
+            {
+                var claims = new List<Claim>
+                {
+                    //ver se a insercao das claims da api vai ser feita no nameidentifier
+                    new Claim(ClaimTypes.NameIdentifier, Input.Email),
+                    new Claim(ClaimTypes.Name, "USERNAME HERE"),
+                    new Claim(ClaimTypes.Role, "ROLE"),
+                    new Claim("RandomDataPoint", "RandomValue")
+                };
+
+                var identityUser = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identityUser);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = Input.RememberMe });
+
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
     }
 }
